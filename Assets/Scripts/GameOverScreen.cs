1a9693a1487cf186d87d945c9fa126ac787abe12
maazjafrani <80782814+maazjafrani@users.noreply.github.com>
2022-09-18 01:53:08 -0700

    add level

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -22,6 +22,14 @@ public class GameOverScreen : MonoBehaviour
         SceneManager.LoadScene(1);
 
     }
+    public void Level2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Level3()
+    {
+        SceneManager.LoadScene(3);
+    }
     public void ExitButton()
     {
          SceneManager.LoadScene(0);
--- Assets/Scripts/GameSuccessScreen.cs
+++ Assets/Scripts/GameSuccessScreen.cs
@@ -21,6 +21,14 @@ public class GameSuccessScreen : MonoBehaviour
         SceneManager.LoadScene(1);
 
     }
+    public void Level2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Level3()
+    {
+        SceneManager.LoadScene(3);
+    }
     public void ExitButton()
     {
         SceneManager.LoadScene(0);
